48fff8d6f515809964e11e0ccb2d05d7b34b4456
jharck <javier.rc@gmail.com>
2017-09-17 13:06:51 -0600

    Sample application for Angular 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'PlatziSquare';
+  lugares:any = [
+  	{ active: false, nombre: 'Floreria Las Gardenias' },
+  	{ active: true, nombre: 'Donas La Pasadita' },
+  	{ active: true, nombre: 'Veterinaria Huellitas' },
+  	{ active: true, nombre: 'Panaderia La Lucerna'},
+  	{ active: true, nombre: 'Carnitas Mama Chuz' },
+  	{ active: false, nombre: 'Panes el Chino' }
+  ];
+
+  constructor(){
+  }
+
 }
